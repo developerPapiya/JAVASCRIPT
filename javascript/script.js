7b495b213413e43790633af74e5395b08b7ffbde
developerPapiya <papiyaro026@gmail.com>
2024-09-23 10:07:37 +0530

    Added fetch API Ex using async-await

--- javascript/script.js
+++ javascript/script.js
@@ -934,3 +934,23 @@ Add a new method called editData to Admin  that allows it to edit website data.*
 // }
  
 
+// <----API---->
+// Using Fetch API
+
+const URL ="https://cat-fact.herokuapp.com/facts"
+const factPara=document.querySelector("#fact");
+
+// let promise = fetch(URL);
+// console.log(promise);
+
+
+ const getFacts = async() => {     //using async-await
+    console.log("Getting data...");
+    let response = await fetch(URL);
+    console.log(response);  //JSON format
+   let data = await response.json();
+   console.log(data[1].text);
+   factPara.innerText = data[1].text;
+
+
+ }
