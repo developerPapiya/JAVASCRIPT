20ca3d42b0c349136b2ed6cc85dc0b2fa9b745cb
developerPapiya <papiyaro026@gmail.com>
2024-09-23 10:22:36 +0530

    Added fetch API using Promise Chain

--- javascript/script.js
+++ javascript/script.js
@@ -937,20 +937,30 @@ Add a new method called editData to Admin  that allows it to edit website data.*
 // <----API---->
 // Using Fetch API
 
+// const URL ="https://cat-fact.herokuapp.com/facts"
+// const factPara=document.querySelector("#fact");
+
+//  const getFacts = async() => {     //using async-await
+//     console.log("Getting data...");
+//     let response = await fetch(URL);
+//     console.log(response);  //JSON format
+//    let data = await response.json();
+//    console.log(data[1].text);
+//    factPara.innerText = data[1].text;
+
+
+// <-- Fetch API using promise Chain-->
+
 const URL ="https://cat-fact.herokuapp.com/facts"
 const factPara=document.querySelector("#fact");
 
-// let promise = fetch(URL);
-// console.log(promise);
+function getFacts() {
+ fetch(URL).then((response) =>{
+  return response.json();
+ })
+ .then((data)=>{
+  console.log(data[1].text);
+  factPara.innerText = data[1].text;
+ })
+}
 
-
- const getFacts = async() => {     //using async-await
-    console.log("Getting data...");
-    let response = await fetch(URL);
-    console.log(response);  //JSON format
-   let data = await response.json();
-   console.log(data[1].text);
-   factPara.innerText = data[1].text;
-
-
- }
